4c8a9fcf4a9deb0561a0500b422a3f4bc95d94ed
mikuinokia <mikuinokia@gmail.com>
2022-06-18 17:56:59 +0900

    テストパターン作成

---
+++ algorithm/search_movie/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# コーディングルールをチェック
+CS_CHECK=$(phpcs -q index.php --standard=../phpcs.xml)
+if [ "$CS_CHECK" = "" ]; then
+  echo "コーディングルール: OK"
+else
+  echo "コーディングルール: NO"
+fi
+
+# テストパターン1から5の出力をチェック
+for i in 1 2 3 4 5
+do
+  echo ./input/input${i} | php index.php > ./output
+
+  # diffで差分がなければOKを表示
+  RESULT=$(diff -q ./expected/expected${i} ./output);
+  if [ "$RESULT" = "" ]; then
+      echo "テストパターン1: OK！"
+  else
+      echo "テストパターン1: NO！"
+  fi
+done
+rm ./output
